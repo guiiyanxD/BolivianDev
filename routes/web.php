28c2f93d4a43a6001e44ed9fa2e61d911737c024
Williams W. Alvarez Zabala <williamswaz18@gmail.com>
2022-11-26 16:50:28 -0400

    fixing online counter error. changes on web.php

--- routes/web.php
+++ routes/web.php
@@ -27,45 +27,49 @@ Route::middleware(['auth'])->group(function() {
     Route::resource('/tipo-participacion', '\App\Http\Controllers\ParticipacionController');
 
 
-/*    Route::any('/board',[\App\Http\Controllers\InviteController::class,'IncrementWhenCodeIsUsed'])
-        ->name('board')
-        ->middleware('protected_by_invite_codes');*/
+
+
+    /**
+     * Routes to create and join to a meet being a host
+     */
+    Route::post('meet/store', [MeetController::class, 'storeAsHost'])
+        ->name('meet.storeAsHost');
+    Route::any('/board', [MeetController::class,'joinToMeet'])
+        ->name('board');
+
+    /**
+     * Routes to join to meet as guest
+     */
+    Route::post('meet/join',[MeetController::class, 'storeAsGuest'])
+        ->name('meet.storeAsGuest');
+
+
+    /**
+     * Route to edit Invitation Code rules and name and description of a meet
+     */
+    Route::get('invitation_code/{meet_id}/{invite_id}/edit', [InviteController::class,'edit'])->name('invite.edit');
+    Route::put('invitation/{meet_id}/{invite_id}/update',[InviteController::class,'update'])->name('invite.update');
+
+    /**
+     * Main route after logged to create a meeting
+     */
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+    /**
+     * Routes to save and update json backup of the meets element
+     */
+    Route::any('meet/backup/update', [BackupController::class,'update'])->name('backup.update');
+    Route::post('meet/backup/load',[BackupController::class,'load'])->name('backup.load');
+
+
+
+    Auth::routes();
+    Broadcast::routes();
+
+
 });
 
-/**
- * Routes to create and join to a meet being a host
- */
-Route::post('meet/store', [MeetController::class, 'storeAsHost'])
-    ->name('meet.storeAsHost');
-Route::any('/board', [MeetController::class,'joinToMeet'])
-    ->name('board');
 
-/**
- * Routes to join to meet as guest
- */
-Route::post('meet/join',[MeetController::class, 'storeAsGuest'])
-    ->name('meet.storeAsGuest');
-
-
-/**
- * Route to edit Invitation Code rules and name and description of a meet
- */
-Route::get('invitation_code/{meet_id}/{invite_id}/edit', [InviteController::class,'edit'])->name('invite.edit');
-Route::put('invitation/{meet_id}/{invite_id}/update',[InviteController::class,'update'])->name('invite.update');
-
-/**
- * Main route after logged to create a meeting
- */
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-/**
- * Routes to save and update json backup of the meets element
- */
-Route::any('meet/backup/update', [BackupController::class,'update'])->name('backup.update');
-Route::post('meet/backup/load',[BackupController::class,'load'])->name('backup.load');
-
-Auth::routes();
-Broadcast::routes();
 //require __DIR__.'/auth.php';
 
